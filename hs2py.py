b48f76a3c1f1bcd8c9bebf20b7c943abe0f1dc71
Leonardo Robol <leo@robol.it>
2025-01-31 18:13:49 +0100

    Rework.

--- hs2py.py
+++ hs2py.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python3
 
-import sys, re
+import sys, re, math
 
 c = []
-p = []
+s = []
 
 with open(sys.argv[1], "r") as h:
+    j = 1
     for line in h:
         ll = re.sub(r",?Harmonic \d+\w*", "", line)
         ll = ll.strip().replace("(", "").replace(")", "").split()
-        c.append(float(ll[1]))
-        p.append(float(ll[0]))
 
-print("p = ", end = "")
-print(p)
+        # Amplitude and phase
+        a = float(ll[1])
+        p = float(ll[0])
+
+        c.append(- a * math.sin(p))
+        s.append(  a * math.cos(p))
+        # c.append(float(ll[1]))
+        # p.append(float(ll[0]))
+
+        j = j + 1
+
 print("c = ", end = "")
 print(c)
+print("s = ", end = "")
+print(s)
