17989ce0861cb9bbf9ef59a252c2cb0a7acb3657
Leonardo Robol <leo@robol.it>
2026-01-22 17:21:52 +0100

    Fix start.sh for notebooks borrowed from Polo2.

--- start.sh
+++ start.sh
@@ -3,10 +3,20 @@
 
 set -e
 
+export PYTHON=python3
+
+#Setup richiesto solo per i PC usati in laboratorio
+if [[ -n "$WSL_DISTRO_NAME" && "$USER" = "studente" ]]; then
+  export PYTHON=python3.10
+  export BROWSER=wslview
+  sudo apt-get -o Acquire::http::Proxy="http://131.114.4.69:3128" update
+  sudo apt-get -y -o Acquire::http::Proxy="http://131.114.4.69:3128" install python3.10-venv wslu
+fi
+
 ENV_LOCATION="/tmp/$USER/env"
 
 if [ ! -d "${ENV_LOCATION}" ]; then
-  python3 -mvenv "${ENV_LOCATION}"
+  ${PYTHON} -mvenv "${ENV_LOCATION}"
 fi
 
 source "${ENV_LOCATION}/bin/activate"
